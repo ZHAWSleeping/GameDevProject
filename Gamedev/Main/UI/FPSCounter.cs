a2f7dd98c8de4bba6023f76b768ca12105cd254f
Jonas Costa <contact@jonascosta.ch>
2024-03-26 00:38:04 +0100

    FPS Counter glow up

--- Gamedev/Main/UI/FPSCounter.cs
+++ Gamedev/Main/UI/FPSCounter.cs
@@ -4,12 +4,21 @@ using System;
 
 namespace Gamedev.Main.UI
 {
-    public partial class FPSCounter : Label
-    {
-        // Called every frame. 'delta' is the elapsed time since the previous frame.
-        public override void _Process(double delta)
-        {
-			Text = "FPS " + Performance.GetMonitor(Performance.Monitor.TimeFps).ToString();
-        }
-    }
+	public partial class FPSCounter : Control
+	{
+		[Export]
+		private Label Counter;
+
+		public override void _Ready()
+		{
+			base._Ready();
+			ProcessMode = ProcessModeEnum.Inherit;
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+			Counter.Text = Performance.GetMonitor(Performance.Monitor.TimeFps).ToString();
+		}
+	}
 }
