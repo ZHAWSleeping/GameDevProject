4d13a630c9e21700032385f17bee0a53ebfcb17f
Nicolas <balkenic@students.zhaw.ch>
2024-03-26 16:50:32 +0100

    palyer walljump

--- Gamedev/Main/Characters/Player/Player.cs
+++ Gamedev/Main/Characters/Player/Player.cs
@@ -3,6 +3,7 @@ using Gamedev.Main.Events;
 using Gamedev.Main.Extensions;
 using Godot;
 using System;
+using System.Diagnostics;
 
 namespace Gamedev.Main.Characters.Player
 {
@@ -12,11 +13,18 @@ namespace Gamedev.Main.Characters.Player
 		private float Speed = 300.0f;
 		[Export]
 		private float JumpVelocity = -400.0f;
+		[Export]
+		private float WallJumpVelocity = -150.0f;
+		[Export]
+		private float WallJumpVelocityX = 300.0f;
 		public override void _Ready()
 		{
 			base._Ready();
 			CollisionEvents.CollisionDeath += Die;
 		}
+
+		private bool canWallJump = true;
+		private bool isFalling = false;
 
 
 		private void Die()
@@ -35,14 +43,26 @@ namespace Gamedev.Main.Characters.Player
 			// Add the gravity.
 			if (!IsOnFloor())
 			{
-				velocity += GetGravity() * (float)delta;
+				if (IsOnWallOnly() && !isFalling)
+				{
+					velocity += new Vector2(0, 600) * (float)delta;
+				}
+				else
+				{
+					velocity += GetGravity() * (float)delta;
+				}
+
 			}
+
+			//needs to change wall or be away from the wall for a sec
 
 			// Handle Jump.
 			if (Input.IsActionJustPressed("Jump") && IsOnFloor())
 			{
 				velocity.Y = JumpVelocity;
 			}
+
+
 
 			// Get the input direction and handle the movement/deceleration.
 			// As good practice, you should replace UI actions with custom gameplay actions.
@@ -54,14 +74,59 @@ namespace Gamedev.Main.Characters.Player
 			);
 			if (direction != Vector2.Zero)
 			{
-				velocity.X = direction.X * Speed;
+				if (!IsOnFloor())
+				{
+					velocity.X += direction.X * Speed / 8;
+				}
+				else
+				{
+					velocity.X = direction.X * Speed;
+				}
+
+				if (MathF.Abs(velocity.X) > Speed)
+				{
+					velocity.X = Speed * Mathf.Sign(velocity.X);
+				}
+
+
+				//if (MathF.Abs(velocity.X) <= (MathF.Abs(direction.X) * Speed))
+
 			}
 			else
 			{
 				velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			}
 
+
+			// Handle Wall Jump.
+			if (Input.IsActionJustPressed("Jump") && IsOnWallOnly() && canWallJump)
+			{
+				float whichSideOnWall;
+				Godot.KinematicCollision2D[] dir = this.GetSlideCollisions();
+
+				if (Position.X - dir[0].GetPosition().X > 0)
+				{
+					whichSideOnWall = WallJumpVelocityX;
+				}
+				else
+				{
+					whichSideOnWall = -WallJumpVelocityX;
+				}
+
+				velocity += new Vector2(whichSideOnWall, WallJumpVelocity);
+				//canWallJump = false;
+
+			}
+
 			Velocity = velocity;
+
+			if (Velocity.Y >= 0)
+			{
+				isFalling = false;
+			}
+			else { isFalling = true; }
+
+
 			MoveAndSlide();
 		}
 	}
