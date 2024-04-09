bebaec335cd9933d4b3b0a50794fa5162a4c4207
Nicolas <balkenic@students.zhaw.ch>
2024-04-09 14:46:36 +0200

    clean up

--- Gamedev/Main/Objects/Boulder.cs
+++ Gamedev/Main/Objects/Boulder.cs
@@ -9,16 +9,13 @@ public partial class Boulder : RigidBody2D
 	private Vector2 Velocity = new Vector2(0, 0);
 
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Velocity.X = speed;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		//ForceUpdateTransform();
 		MoveAndCollide((float)delta * Velocity);
 	}
 }
